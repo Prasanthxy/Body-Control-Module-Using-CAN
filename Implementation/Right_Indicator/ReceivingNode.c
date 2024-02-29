f026032f7add7f74aa3e6c89c27d1e8833b0a974
Durga Prasanth <98865606+Prasanthxy@users.noreply.github.com>
2024-02-29 19:52:14 +0530

    Updated

---
+++ Implementation/Right_Indicator/ReceivingNode.c
@@ -0,0 +1,29 @@
+#include"CANheader.h"
+#define LED 1<<17
+
+int main()
+{
+	CAN2_MSG m1; //Structure Variables
+	unsigned int i;
+	CAN_Config();
+	UART0_Config(115200);
+	IODIR0=LED;
+	IOSET0=LED;
+	UART0_String("Testing\r\n");
+	while(1)
+	{		
+		CAN_Rx(&m1);
+		UART0_String("CAN Received:\r\n");
+		UART0_Hex(m1.id);
+		if(m1.id==0x20)
+		{
+			for(i=0;i<10;i++)
+			{
+				IOCLR0=LED;
+				delay_milliseconds(500);
+				IOSET0=LED;
+				delay_milliseconds(500);
+			}
+		}		
+	}
+}
